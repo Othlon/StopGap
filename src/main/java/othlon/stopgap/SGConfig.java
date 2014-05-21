069f2b371db0575c181b2aab9ef7bbf7a85a3484
Othlon <jen@sutherland-studios.com.au>
2014-05-21 15:14:48 +1000

    StopGap! baby StopGap! starting the config file!

---
+++ src/main/java/othlon/stopgap/SGConfig.java
@@ -0,0 +1,19 @@
+package othlon.stopgap;
+
+/**
+ * Created by Jen on 20/05/14.
+ */
+
+import java.io.File;
+
+import net.minecraftforge.common.config.Configuration;
+import net.minecraftforge.common.config.Property;
+
+public class SGConfig {
+
+    public static void initConf()
+    {
+
+    }
+
+}
