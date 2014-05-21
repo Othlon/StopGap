62e1c06ec5ab60d307e636de310427296016b2a8
Othlon <jen@sutherland-studios.com.au>
2014-05-21 15:17:06 +1000

    StopGap! baby StopGap! just the ingot base class

---
+++ src/main/java/othlon/stopgap/items/SGIngot.java
@@ -0,0 +1,13 @@
+package othlon.stopgap.items;
+
+import net.minecraft.item.Item;
+import net.minecraft.creativetab.CreativeTabs;
+import othlon.stopgap.StopGap;
+
+/**
+ * Created by Jen on 18/05/14.
+ */
+public class SGIngot extends Item{
+
+
+}
