ec6ae2ad0dea075abb91b3996261e6efd22e7f74
Othlon <jen@sutherland-studios.com.au>
2014-06-26 17:40:37 +1000

    updated version #

--- src/main/java/othlon/stopgap/StopGap.java
+++ src/main/java/othlon/stopgap/StopGap.java
@@ -15,7 +15,7 @@ public class StopGap {
 
     public static final String MODID   = "StopGap";
     public static final String MODNAME = "StopGap";
-    public static final String VERSION = "1.8";
+    public static final String VERSION = "2.0";
 
     private static CreativeTabs stopGapTab = new SGTab(CreativeTabs.getNextID(),MODID );
 
